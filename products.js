d3d3f4f6eefb1f9e1fc4ad9be7d76d35b8322de5
Gabriel Mania <mania.gabb@gmail.com>
2022-10-09 13:59:16 +0800

    add products.js with data

---
+++ products.js
@@ -0,0 +1,57 @@
+const bikes = [
+  {
+    brand: "Dartmoor",
+    model: "Primal Pro",
+    price: 110000,
+  },
+  {
+    brand: "Dartmoor",
+    model: "Blackbird Evo",
+    price: 150000,
+  },
+  {
+    brand: "Dartmoor",
+    model: "Hornet Pro",
+    price: 129000,
+  },
+  {
+    brand: "Giant",
+    model: "TCR Advance Pro",
+    price: 115000,
+  },
+  {
+    brand: "Giant",
+    model: "Trance X",
+    price: 119000,
+  },
+  {
+    brand: "Giant",
+    model: "XTR SLR",
+    price: 60000,
+  },
+  {
+    brand: "GT",
+    model: "Fury Pro",
+    price: 360000,
+  },
+  {
+    brand: "GT",
+    model: "Sensor Sport",
+    price: 112000,
+  },
+  {
+    brand: "Trek",
+    model: "Emonda SLR eTap",
+    price: 395000,
+  },
+  {
+    brand: "Trek",
+    model: "Procaliber 9.5",
+    price: 117000,
+  },
+  {
+    brand: "Trek",
+    model: "Top Fuel 7",
+    price: 208000,
+  },
+];
